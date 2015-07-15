faa140bfe7f88aecb2f74ed9609c75d509762454
Joseph Leon <joseph.leon.9@gmail.com>
2015-07-15 08:43:08 -0400

    Import webkit, set var webView, create loadView function and add WKNavigationDelegate protocol onto ViewController class.

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
